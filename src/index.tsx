3a1819d19d079ad791aa28030de71ccaaab82aa0
Miguel Angel Toledo Santiago <53960931+Geaglts@users.noreply.github.com>
2021-09-20 11:55:57 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from 'react-dom';
+import Home from './pages/Home';
+
+render(<Home />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  devtool: 'source-map',
+  devServer: { port: 5000, historyApiFallback: true },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.tsx?$/, exclude: /node_modules/, use: ['ts-loader'] },
+      { test: /\.s?css$/, use: ['css-loader', 'sass-loader'] },
+    ],
+  },
+  plugins: [
+    new HTMLPlugin({ template: path.join(__dirname, 'public', 'index.html') }),
+    new CleanWebpackPlugin(),
+  ],
+};
